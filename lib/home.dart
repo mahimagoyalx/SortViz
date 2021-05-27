82c5b782ac233fadbec4b9dbf3805574009f743d
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-05-27 20:47:12 +0530

    Create separate service layer to hold the business logic

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:sortviz/components/barPainter.dart';
 import 'package:sortviz/components/cardView.dart';
+import 'package:sortviz/service/sorting_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -16,22 +16,24 @@ class _HomeScreenState extends State<HomeScreen> {
   Color sortColor = Colors.blue;
   int c = 0;
   List<Color> colors = List.generate(3, (index) => null);
-
+  SortingService sortingService;
   int s = 0;
-  int arraysize = 200;
+  int size = 200;
   var duration = 500;
   String dropdownValue = "Merge Sort";
   List<int> nums = [];
-  StreamController<List<int>> _streamController;
-  Stream<List<int>> _stream;
+  Stream<List<int>> stream;
 
   @override
   void initState() {
     super.initState();
+    sortingService = SortingService(
+      size: size,
+      microsecondDuration: duration,
+    );
+
+    stream = sortingService.stream;
     _makeColors();
-    _streamController = StreamController<List<int>>();
-    _stream = _streamController.stream;
-    randomise();
   }
 
   _makeColors() {
@@ -81,217 +83,6 @@ class _HomeScreenState extends State<HomeScreen> {
         });
         break;
     }
-  }
-
-  menu() {}
-
-  randomise() {
-    nums.clear();
-    for (int i = 0; i < arraysize; i++) {
-      nums.add(Random().nextInt(490) + 10);
-    }
-    _streamController.add(nums);
-  }
-
-  // Sorting Algorithms
-
-  // Bubble Sort
-  bubblesort() async {
-    for (int i = 0; i < nums.length; ++i) {
-      for (int j = 0; j < nums.length - i - 1; ++j) {
-        if (nums[j] > nums[j + 1]) {
-          int temp = nums[j];
-          nums[j] = nums[j + 1];
-          nums[j + 1] = temp;
-        }
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-      }
-    }
-  }
-
-  // Selection Sort
-  selectionSort() async {
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] > nums[j]) {
-          int temp = nums[j];
-          nums[j] = nums[i];
-          nums[i] = temp;
-        }
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-      }
-    }
-  }
-
-  // Insertion Sort
-  insertionSort() async {
-    for (int i = 1; i < nums.length; i++) {
-      int temp = nums[i];
-      int j = i - 1;
-      while (j >= 0 && temp < nums[j]) {
-        nums[j + 1] = nums[j];
-        --j;
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-      }
-      nums[j + 1] = temp;
-      await Future.delayed(Duration(microseconds: duration));
-      _streamController.add(nums);
-    }
-  }
-
-  // Merge Sort
-  mergeSort(int leftIndex, int rightIndex) async {
-    Future<void> merge(int leftIndex, int middleIndex, int rightIndex) async {
-      int leftSize = middleIndex - leftIndex + 1;
-      int rightSize = rightIndex - middleIndex;
-
-      List leftList = new List(leftSize);
-      List rightList = new List(rightSize);
-
-      for (int i = 0; i < leftSize; i++) leftList[i] = nums[leftIndex + i];
-      for (int j = 0; j < rightSize; j++)
-        rightList[j] = nums[middleIndex + j + 1];
-
-      int i = 0, j = 0;
-      int k = leftIndex;
-
-      while (i < leftSize && j < rightSize) {
-        if (leftList[i] <= rightList[j]) {
-          nums[k] = leftList[i];
-          i++;
-        } else {
-          nums[k] = rightList[j];
-          j++;
-        }
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-        k++;
-      }
-
-      while (i < leftSize) {
-        nums[k] = leftList[i];
-        i++;
-        k++;
-
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-      }
-
-      while (j < rightSize) {
-        nums[k] = rightList[j];
-        j++;
-        k++;
-
-        await Future.delayed(Duration(microseconds: duration));
-        _streamController.add(nums);
-      }
-    }
-
-    if (leftIndex < rightIndex) {
-      int middleIndex = (rightIndex + leftIndex) ~/ 2;
-
-      await mergeSort(leftIndex, middleIndex);
-      await mergeSort(middleIndex + 1, rightIndex);
-
-      await Future.delayed(Duration(microseconds: duration));
-
-      _streamController.add(nums);
-
-      await merge(leftIndex, middleIndex, rightIndex);
-    }
-  }
-
-  // Quick Sort
-  quickSort(int leftIndex, int rightIndex) async {
-    Future<int> _partition(int left, int right) async {
-      int p = (left + (right - left) / 2).toInt();
-
-      var temp = nums[p];
-      nums[p] = nums[right];
-      nums[right] = temp;
-      await Future.delayed(Duration(microseconds: duration));
-
-      _streamController.add(nums);
-
-      int cursor = left;
-
-      for (int i = left; i < right; i++) {
-        if (cf(nums[i], nums[right]) <= 0) {
-          var temp = nums[i];
-          nums[i] = nums[cursor];
-          nums[cursor] = temp;
-          cursor++;
-
-          await Future.delayed(Duration(microseconds: duration));
-
-          _streamController.add(nums);
-        }
-      }
-
-      temp = nums[right];
-      nums[right] = nums[cursor];
-      nums[cursor] = temp;
-
-      await Future.delayed(Duration(microseconds: duration));
-
-      _streamController.add(nums);
-
-      return cursor;
-    }
-
-    if (leftIndex < rightIndex) {
-      int p = await _partition(leftIndex, rightIndex);
-
-      await quickSort(leftIndex, p - 1);
-
-      await quickSort(p + 1, rightIndex);
-    }
-  }
-
-  cf(int a, int b) {
-    if (a < b) {
-      return -1;
-    } else if (a > b) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
-  // Heap Sort
-  heapSort() async {
-    for (int i = nums.length ~/ 2; i >= 0; i--) {
-      await heapify(nums, nums.length, i);
-      _streamController.add(nums);
-    }
-    for (int i = nums.length - 1; i >= 0; i--) {
-      int temp = nums[0];
-      nums[0] = nums[i];
-      nums[i] = temp;
-      await heapify(nums, i, 0);
-      _streamController.add(nums);
-    }
-  }
-
-  heapify(List<int> arr, int n, int i) async {
-    int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
-
-    if (l < n && arr[l] > arr[largest]) largest = l;
-
-    if (r < n && arr[r] > arr[largest]) largest = r;
-
-    if (largest != i) {
-      int temp = nums[i];
-      nums[i] = nums[largest];
-      nums[largest] = temp;
-      heapify(arr, n, largest);
-    }
-    await Future.delayed(Duration(microseconds: duration));
   }
 
   @override
@@ -397,24 +188,29 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: EdgeInsets.all(0),
         child: Column(
           children: [
-            StreamBuilder<Object>(
-                stream: _stream,
-                builder: (context, snapshot) {
-                  int ind = 0;
-                  return Row(
-                    children: nums.map((int number) {
+            StreamBuilder<List<int>>(
+              stream: stream,
+              builder: (context, snapshot) {
+                List<int> arr = snapshot.data;
+
+                int ind = 0;
+                return Row(
+                  children: arr.map(
+                    (int number) {
                       ind++;
                       return CustomPaint(
                         painter: ArrayBar(
                           sortColor: sortColor,
-                          width: MediaQuery.of(context).size.width / arraysize,
+                          width: MediaQuery.of(context).size.width / size,
                           height: number,
                           index: ind,
                         ),
                       );
-                    }).toList(),
-                  );
-                }),
+                    },
+                  ).toList(),
+                );
+              },
+            ),
             Padding(
               padding: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height - 190,
@@ -466,22 +262,22 @@ class _HomeScreenState extends State<HomeScreen> {
                       onPressed: () async {
                         switch (s) {
                           case 0:
-                            await mergeSort(0, arraysize - 1);
+                            await sortingService.mergeSort(0, size - 1);
                             break;
                           case 1:
-                            await quickSort(0, arraysize - 1);
+                            await sortingService.quickSort(0, size - 1);
                             break;
                           case 2:
-                            selectionSort();
+                            sortingService.selectionSort();
                             break;
                           case 3:
-                            bubblesort();
+                            sortingService.bubbleSort();
                             break;
                           case 4:
-                            insertionSort();
+                            sortingService.insertionSort();
                             break;
                           case 5:
-                            await heapSort();
+                            await sortingService.heapSort();
                             break;
                         }
                       },
@@ -500,7 +296,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     height: 55,
                     child: TextButton(
                       style: TextButton.styleFrom(primary: Colors.white),
-                      onPressed: randomise,
+                      onPressed: sortingService.randomise,
                       child: Icon(
                         Icons.shuffle,
                         size: 24,
@@ -554,7 +350,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                                       MainAxisAlignment.center,
                                                   children: <Widget>[
                                                     Text(
-                                                      arraysize.toString(),
+                                                      size.toString(),
                                                       style: TextStyle(
                                                         fontSize: 24.0,
                                                         fontWeight:
@@ -585,13 +381,13 @@ class _HomeScreenState extends State<HomeScreen> {
                                                   child: Slider(
                                                     max: 500,
                                                     min: 5,
-                                                    value: arraysize.toDouble(),
+                                                    value: size.toDouble(),
                                                     inactiveColor: Colors.white,
                                                     onChanged: (double value) {
                                                       setState(() {
-                                                        arraysize =
-                                                            value.round();
-                                                        randomise();
+                                                        size = value.round();
+                                                        sortingService
+                                                            .randomise();
                                                       });
                                                     },
                                                   ),
---
+++ lib/service/sorting_service.dart
@@ -0,0 +1,237 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:meta/meta.dart';
+
+class SortingService {
+  SortingService({@required int size, @required int microsecondDuration})
+      : this._size = size,
+        this._duration = microsecondDuration,
+        assert(size != null),
+        assert(microsecondDuration != null) {
+    randomise();
+  }
+
+  final int _size;
+  final int _duration;
+  final List<int> arr = [];
+  final StreamController<List<int>> _streamController =
+      StreamController<List<int>>();
+
+  int get size => _size;
+
+  Stream<List<int>> get stream => _streamController.stream;
+
+  void dispose() {
+    _streamController.close();
+  }
+
+  randomise() {
+    arr.clear();
+    for (int i = 0; i < _size; i++) {
+      arr.add(Random().nextInt(490) + 10);
+    }
+    _streamController.add(arr);
+  }
+
+  // Sorting Algorithms
+
+  // Bubble Sort
+  void bubbleSort() async {
+    for (int i = 0; i < arr.length; ++i) {
+      for (int j = 0; j < arr.length - i - 1; ++j) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+      }
+    }
+  }
+
+  // Selection Sort
+  selectionSort() async {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          int temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+        }
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+      }
+    }
+  }
+
+  // Insertion Sort
+  insertionSort() async {
+    for (int i = 1; i < arr.length; i++) {
+      int temp = arr[i];
+      int j = i - 1;
+      while (j >= 0 && temp < arr[j]) {
+        arr[j + 1] = arr[j];
+        --j;
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+      }
+      arr[j + 1] = temp;
+      await Future.delayed(Duration(microseconds: _duration));
+      _streamController.add(arr);
+    }
+  }
+
+  // Merge Sort
+  mergeSort(int leftIndex, int rightIndex) async {
+    Future<void> merge(int leftIndex, int middleIndex, int rightIndex) async {
+      int leftSize = middleIndex - leftIndex + 1;
+      int rightSize = rightIndex - middleIndex;
+
+      List<int> leftList = List.generate(leftSize, (index) => null);
+      List<int> rightList = List.generate(rightSize, (index) => null);
+
+      for (int i = 0; i < leftSize; i++) leftList[i] = arr[leftIndex + i];
+      for (int j = 0; j < rightSize; j++)
+        rightList[j] = arr[middleIndex + j + 1];
+
+      int i = 0, j = 0;
+      int k = leftIndex;
+
+      while (i < leftSize && j < rightSize) {
+        if (leftList[i] <= rightList[j]) {
+          arr[k] = leftList[i];
+          i++;
+        } else {
+          arr[k] = rightList[j];
+          j++;
+        }
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+        k++;
+      }
+
+      while (i < leftSize) {
+        arr[k] = leftList[i];
+        i++;
+        k++;
+
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+      }
+
+      while (j < rightSize) {
+        arr[k] = rightList[j];
+        j++;
+        k++;
+
+        await Future.delayed(Duration(microseconds: _duration));
+        _streamController.add(arr);
+      }
+    }
+
+    if (leftIndex < rightIndex) {
+      int middleIndex = (rightIndex + leftIndex) ~/ 2;
+
+      await mergeSort(leftIndex, middleIndex);
+      await mergeSort(middleIndex + 1, rightIndex);
+
+      await Future.delayed(Duration(microseconds: _duration));
+
+      _streamController.add(arr);
+
+      await merge(leftIndex, middleIndex, rightIndex);
+    }
+  }
+
+  // Quick Sort
+  quickSort(int leftIndex, int rightIndex) async {
+    Future<int> _partition(int left, int right) async {
+      int p = (left + (right - left) / 2).toInt();
+
+      var temp = arr[p];
+      arr[p] = arr[right];
+      arr[right] = temp;
+      await Future.delayed(Duration(microseconds: _duration));
+
+      _streamController.add(arr);
+
+      int cursor = left;
+
+      for (int i = left; i < right; i++) {
+        if (cf(arr[i], arr[right]) <= 0) {
+          var temp = arr[i];
+          arr[i] = arr[cursor];
+          arr[cursor] = temp;
+          cursor++;
+
+          await Future.delayed(Duration(microseconds: _duration));
+
+          _streamController.add(arr);
+        }
+      }
+
+      temp = arr[right];
+      arr[right] = arr[cursor];
+      arr[cursor] = temp;
+
+      await Future.delayed(Duration(microseconds: _duration));
+
+      _streamController.add(arr);
+
+      return cursor;
+    }
+
+    if (leftIndex < rightIndex) {
+      int p = await _partition(leftIndex, rightIndex);
+
+      await quickSort(leftIndex, p - 1);
+
+      await quickSort(p + 1, rightIndex);
+    }
+  }
+
+  cf(int a, int b) {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  // Heap Sort
+  heapSort() async {
+    for (int i = arr.length ~/ 2; i >= 0; i--) {
+      await heapify(arr, arr.length, i);
+      _streamController.add(arr);
+    }
+    for (int i = arr.length - 1; i >= 0; i--) {
+      int temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      await heapify(arr, i, 0);
+      _streamController.add(arr);
+    }
+  }
+
+  heapify(List<int> arr, int n, int i) async {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest]) largest = l;
+
+    if (r < n && arr[r] > arr[largest]) largest = r;
+
+    if (largest != i) {
+      int temp = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+      heapify(arr, n, largest);
+    }
+    await Future.delayed(Duration(microseconds: _duration));
+  }
+}
